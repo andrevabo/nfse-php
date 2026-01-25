e542d53792cf8631a18c45455ccf59b237197cce
a21ns1g4ts <atila.danvi@outlook.com>
2026-01-25 02:28:08 -0300

    feat: add ErroPreenchimento case to MotivoSubstituicao enum.

--- src/Enums/MotivoSubstituicao.php
+++ src/Enums/MotivoSubstituicao.php
@@ -25,6 +25,11 @@ enum MotivoSubstituicao: string
     case Outros = '99';
 
     /**
+     * Erro no Preenchimento
+     */
+    case ErroPreenchimento = '05';
+
+    /**
      * Get description for the enum case
      */
     public function getDescription(): string
@@ -32,6 +37,7 @@ enum MotivoSubstituicao: string
         return match ($this) {
             self::DesenquadramentoSimplesNacional => 'Desenquadramento de NFS-e do Simples Nacional',
             self::EnquadramentoSimplesNacional => 'Enquadramento de NFS-e no Simples Nacional',
+            self::ErroPreenchimento => 'Erro no Preenchimento',
             self::Outros => 'Outros',
         };
     }
