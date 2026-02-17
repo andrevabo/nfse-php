f8081ad60cbbcea3b6924d1cd8383e7778650631
a21ns1g4ts <atila.danvi@outlook.com>
2026-02-17 03:31:46 -0300

    feat: Add support for federal, state, and municipal percentage-based total tributes in DPS XML.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,11 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 set_error_handler(function ($severity, $message, $file, $line) {
     if ($severity === E_DEPRECATED && str_contains($message, 'setAccessible')) {
         return true; // suppress
     }
+
     return false; // bubble up
 });
